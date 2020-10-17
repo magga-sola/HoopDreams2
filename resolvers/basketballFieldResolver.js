606be3f7ffff8c4c82fac4e0465673840a3ab5ad
m4gga <margriette123@gmail.com>
2020-10-17 15:00:43 +0000

    git og basketballFieldResolver

--- resolvers/basketballFieldResolver.js
+++ resolvers/basketballFieldResolver.js
@@ -1,8 +1,23 @@
 // • (5%) allBasketballFields - Should return a collection of all basketball fields. Contains a field argument called status which is of type BasketballFieldStatus (enum) and should be used to filter the data based on the status of the basketball field
 // • (5%) basketballField - Should return a specific basketball field by id
+
 const errors = require("../errors");
 const fields = require("../services/basketballFieldService")
 
 module.exports = {
+    queries: {
+        allBasketballFields: async (parent, args) => {
+            return 0;
+        },
+        basketballFields: async (parent, args) => {
+            return 0;
+        }
 
+    },
+    types: {
+        BasketballField: {
+            pickupGames: async (parent, args, {db}) => (await db.PickupGame.find({})).filter(b => b.basketballFieldId === parent.id)
+        }
+
+    }
 };
