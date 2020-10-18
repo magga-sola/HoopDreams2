5c4b618aa650de169c501a3650580d0cd6576b7e
EvaNaabye <evan19@ru.is>
2020-10-18 23:49:40 +0000

    f. skil

--- services/basketballFieldService.js
+++ services/basketballFieldService.js
@@ -1,12 +1,12 @@
-//const request = require("request");
+const request = require("request");
 
-//var fields = request.get("https://basketball-fields.herokuapp.com/api/basketball-fields", {json: true}, (err, res, body) => {
-//    if (err) {
-//        return console.log(err);
-//    }
-//    return body;
-//})
+var fields = request.get("https://basketball-fields.herokuapp.com/api/basketball-fields", {json: true}, (err, res, body) => {
+    if (err) {
+        return console.log(err);
+    }
+    return body;
+})
 
-//module.exports = {
-//    basketballFields: fields
-//}
+module.exports = {
+    basketballFields: fields
+}
