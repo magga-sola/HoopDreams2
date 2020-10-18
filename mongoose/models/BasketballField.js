710a64574e361be12ab7528912d2e870c743e6fe
m4gga <margriette123@gmail.com>
2020-10-18 22:25:09 +0000

    lagadi id villu

--- mongoose/models/BasketballField.js
+++ mongoose/models/BasketballField.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose");
 
 module.exports = new mongoose.Schema({
-    id: { type: String, required: true },
     name: { type: String, required: true },
     capacity: { type: Number, required: true },
     yearOfCreation: { type: String, required: true},
